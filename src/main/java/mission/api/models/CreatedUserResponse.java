dbe06948199837ef7a024ca91078c2763673cd0f
Mayur Karnik <mayurkarnik.qe@gmail.com>
2026-03-22 13:33:27 +0530

    feat: add UserData and CreatedUserResponse model classes for JSON deserialization

--- src/main/java/mission/api/models/CreatedUserResponse.java
+++ src/main/java/mission/api/models/CreatedUserResponse.java
@@ -1,4 +1,36 @@
 package mission.api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CreatedUserResponse {
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("job")
+    private String job;
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("createdAt")
+    private String createdAt;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
 }
--- src/main/java/mission/api/models/UserData.java
+++ src/main/java/mission/api/models/UserData.java
@@ -1,4 +1,37 @@
 package mission.api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserData {
+
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    @JsonProperty("email")
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
